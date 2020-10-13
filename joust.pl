5e0713186ceac0714c9622d13e18f15dacdd0f15
carlsbad <carlsbad@sentinelone.com>
2020-10-13 16:58:54 +0300

    Use a separate module for defining all the valid moves

--- joust.pl
+++ joust.pl
@@ -1,4 +1,5 @@
 :- use_module(library(pce)).
+:- use_module(moves).
 :- dynamic user_choice/1.
 
 read_game_config :-
@@ -105,62 +106,6 @@ play(state(Board, WhitePos, BlackPos, black)) :-
 % respectively
 % Turn - an atom which can take the values { white, black }
 
-knight_move(X/Y, X1/Y1) :-
-    % Move North-East.
-    X1 is X + 1,
-    Y1 is Y + 2.
-
-knight_move(X/Y, X1/Y1) :-
-    % Move East-North.
-    X1 is X + 2,
-    Y1 is Y + 1.
-
-knight_move(X/Y, X1/Y1) :-
-    % Move East-South.
-    X1 is X + 2,
-    Y1 is Y - 1.
-
-knight_move(X/Y, X1/Y1) :-
-    % Move South-East.
-    X1 is X + 1,
-    Y1 is Y - 2.
-
-knight_move(X/Y, X1/Y1) :-
-    % Move North-West.
-    X1 is X - 1,
-    Y1 is Y - 2.
-
-knight_move(X/Y, X1/Y1) :-
-    % Move West-South.
-    X1 is X - 2,
-    Y1 is Y - 1.
-
-knight_move(X/Y, X1/Y1) :-
-    % Move West-North.
-    X1 is X - 2,
-    Y1 is Y + 1.
-
-knight_move(X/Y, X1/Y1) :-
-    % Move North-West.
-    X1 is X - 1,
-    Y1 is Y + 2.
-
-move(state(Board, WhitePos, BlackPos, white), state(Board1, WhitePos1, BlackPos, black)) :-
-    % White moves.
-    delete(Board, WhitePos, Board1),
-    knight_move(WhitePos, WhitePos1),
-    member(WhitePos1, Board1).
-
-move(state(Board, WhitePos, BlackPos, black), state(Board1, WhitePos, BlackPos1, white)) :-
-    % Black moves.
-    delete(Board, BlackPos, Board1),
-    knight_move(BlackPos, BlackPos1),
-    member(BlackPos1, Board1).
-
-% Generates a list of all legal moves from a given state.
-% Fails if no moves are legal.
-moves(State, [M1|Ms]) :-
-    findall(State1, move(State, State1), [M1|Ms]).
 
 square_colour(X/Y, brown) :-
     (X + Y) mod 2 =:= 0, !.
